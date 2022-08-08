4bed68b43e464deda6a2efa410d2f984f3d70573
Key Yu Wan <keyflcbyuwan@gmail.com>
2022-08-08 20:22:22 -0300

    feat(splash): animated splash screen

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,49 +1,75 @@
-import React from "react";
-import { Button, StyleSheet, Dimensions } from "react-native";
+import React, { useEffect } from "react";
 import Animated, {
-  useSharedValue,
   useAnimatedStyle,
+  useSharedValue,
   withTiming,
-  Easing,
+  interpolate,
+  Extrapolate,
 } from "react-native-reanimated";
+
+import BrandSvg from "../../assets/brand.svg";
+import LogoSvg from "../../assets/logo.svg";
 
 import { Container } from "./styles";
 
-const DEVICE_WIDTH = Dimensions.get("window").width;
-
 export function Splash() {
-  const animated = useSharedValue(0);
+  const splashAnimation = useSharedValue(0);
 
-  const animatedStyles = useAnimatedStyle(() => {
+  const brandStyle = useAnimatedStyle(() => {
     return {
+      opacity: interpolate(
+        splashAnimation.value,
+        [0, 50], // momento da animação
+        [1, 0] // valor da opacidade
+      ),
       transform: [
         {
-          translateX: withTiming(animated.value, {
-            duration: 500,
-            easing: Easing.bezier(0.05, 0.44, 0.91, 0.68),
-          }),
+          translateX: interpolate(
+            splashAnimation.value,
+            [0, 50],
+            [0, -50],
+            Extrapolate.CLAMP // nunca passa do limite que eu inpûs na animação
+          ),
         },
       ],
     };
   });
 
-  function handleMove() {
-    animated.value = Math.random() * (DEVICE_WIDTH - 100);
-  }
+  const logoStyle = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(
+        splashAnimation.value,
+        [0, 25, 50], // momento da animação
+        [0, 0.3, 1] // valor da opacidade
+      ),
+      transform: [
+        {
+          translateX: interpolate(
+            splashAnimation.value,
+            [0, 50],
+            [-50, 0],
+            Extrapolate.CLAMP
+          ),
+        },
+      ],
+    };
+  });
+
+  useEffect(() => {
+    splashAnimation.value = withTiming(50, {
+      duration: 1000,
+    });
+  }, []);
 
   return (
     <Container>
-      <Animated.View style={[styles.box, animatedStyles]} />
+      <Animated.View style={[brandStyle, { position: "absolute" }]}>
+        <BrandSvg width={80} height={50} />
+      </Animated.View>
 
-      <Button title="Mover" onPress={handleMove} />
+      <Animated.View style={[logoStyle, { position: "absolute" }]}>
+        <LogoSvg width={180} height={20} />
+      </Animated.View>
     </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  box: {
-    width: 100,
-    height: 100,
-    backgroundColor: "purple",
-  },
-});
